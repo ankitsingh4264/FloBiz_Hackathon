161ddc7ce8f53da9bc2fd306ddadba0ef5baeb66
ankit singh <singh.ankit2117@gmail.com>
2021-10-28 01:07:26 +0530

    Code Optimization and concurrency handling search and tag filter sync

--- app/src/main/java/com/example/flobizhackathon/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/example/flobizhackathon/ui/MainActivityViewModel.kt
@@ -23,17 +23,22 @@ class MainActivityViewModel @Inject constructor(private val dataRepository: Data
     private val set=HashSet<String>()
     var tagList=ArrayList<String>()
     var clickedTag:String=""
-    var job:Job?=null
+    private var job:Job?=null
     var id=0;
+
     suspend fun getData(){
          val list=ArrayList<Items>()
-
-         job?.join()
-         job=viewModelScope.launch {
-            dataRepository.getRemoteData().collect {
+        var loading=false
+        job?.let {
+            if (job!!.isActive){
+               loading=true
+            }
+        }
+        if (loading) return
+        job=viewModelScope.launch {
+             dataRepository.getRemoteData().collect {
                 if (it.items==null) return@collect
                 for (temp in it.items!!) {
-                    Log.d("ankit", "quesid: ${temp?.questionId} ")
                     temp?.questionId?.let {
                         if (!set.contains(it)) {
                             list.add(temp)
